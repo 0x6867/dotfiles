bcb152c9cfdd2204a498cd7d85096f5410a99d44
0x6867 <1156977+0x6867@users.noreply.github.com>
2026-07-22 23:18:12 -0400

    added macbook stuff. Not complete. intel macs not supported after 26.05

--- flake.nix
+++ flake.nix
@@ -7,16 +7,21 @@
       # Official NixOS, Darwin, HM Package Sources
       # Source Packages
       nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
+      nixpkgs_mac.url = "github:nixos/nixpkgs?ref=nixos-26.05";
 
       # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
       # nixos-hardware.url = "github:nixos/nixos-hardware"
 
+      # Darwin (Mac) Nix package manager
+      darwin = {
+	url = "github:nix-darwin/nix-darwin";
+	inputs.nixpkgs.follows = "nixpkgs";
+      };
       # Home manager
       home-manager = {
       	url = "github:nix-community/home-manager/release-26.05";
         inputs.nixpkgs.follows = "nixpkgs";
       };
-
 
       # Utilities
       # Disko for declaritive partioning and formatting
@@ -30,7 +35,8 @@
         url = "github:nix-community/nix-index-database";
         inputs.nixpkgs.follows = "nixpkgs";
       };
-
+     
+     # For Secrets Management (unused atm)
      sopsnix = {
         url = "github:mic92/sops-nix";
         inputs.nixpkgs.follows = "nixpkgs";
@@ -40,7 +46,7 @@
      
    };
 
-   outputs = { self, nixpkgs, home-manager, disko, ... }: {
+   outputs = { self, nixpkgs, darwin, home-manager, disko, ... }: {
       nixosConfigurations = {
          desktop-nix = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
@@ -57,6 +63,21 @@
                }
             ];
          };
+      };
+      darwinConfigurations = {
+         macbook-nix = darwin.lib.darwinSystem {
+	   system = "x86_64-darwin";
+	   modules = [
+	     ./hosts/macbook/darwin-configuration.nix
+	     home-manager.darwinModules.home-manager {
+	       home-manager = {
+	         useGlobalpkgs = true;
+		 useUserPackages = true;
+		 users.nix_dev = ./home/macbook.nix;
+	       };
+	     }
+	   ];
+	};
       };
    };
 }
